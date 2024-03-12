9de0d42bd15b3fccef29e2dc22545f4a5ef35f81
Fikri Ramadan <146225946+fikriZ12@users.noreply.github.com>
2024-03-12 12:20:41 +0700

    hasilll.php

--- hasilll.php
+++ hasilll.php
@@ -38,8 +38,46 @@ foreach ($skill as $data) {
     }
 }
 //Tampilan value kedalam web browser
+echo "<style>
+        table {
+            border-collapse: collapse;
+            width: 50%;
+            margin-top: 20px;
+        }
+        th, td {
+            padding: 10px;
+            text-align: left;
+        }
+        th {
+            background-color: #4CAF50;
+            color: white;
+        }
+        tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+        td {
+            border: 1px solid #dddddd;
+        }
+        .tidak-memadai {
+            color: red;
+        }
+        .kurang {
+            color: orange;
+        }
+        .cukup {
+            color: yellow;
+        }
+        .baik {
+            color: green;
+        }
+        .sangat-baik {
+            color: darkgreen;
+        }
+    </style>";
+
 echo "<h4>Informasi Yang Dikirim</h4>";
 echo "<table border='1'>";
+echo "<tr><th>Detail</th><th>Informasi</th></tr>";
 echo "<tr><td>NIM</td><td>$nim</td></tr>";
 echo "<tr><td>Nama</td><td>$nama</td></tr>";
 echo "<tr><td>Jenis Kelamin</td><td>$jk</td></tr>";
@@ -49,14 +87,18 @@ echo "<tr><td>Domisili</td><td>$domisili</td></tr>";
 echo "<tr><td>Email</td><td>$email</td></tr>";
 echo "<tr><td>Nilai skill</td><td>$nilai</td></tr>";
 echo "<tr><td>Keterangan</td><td>";
+
 if ($nilai == 0) {
-    echo "Tidak memadai";
+    echo "<span class='tidak-memadai'>Tidak memadai</span>";
 } elseif ($nilai <= 40) {
-    echo "Kurang";
+    echo "<span class='kurang'>Kurang</span>";
 } elseif ($nilai <= 60) {
-    echo "Cukup";
+    echo "<span class='cukup'>Cukup</span>";
 } elseif ($nilai <= 100) {
-    echo "Baik";
+    echo "<span class='baik'>Baik</span>";
 } elseif ($nilai <= 150) {
-    echo "Sangat baik";
+    echo "<span class='sangat-baik'>Sangat baik</span>";
 }
+
+echo "</td></tr>";
+echo "</table>";
